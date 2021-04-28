88e97ba9d260938f060751e81be46b527bc8264f
Arman Manjian <arman_manjian@edu.aua.am>
2021-04-28 14:51:37 -0400

    AgeFiboByCppLastVersion

--- AgeFibNumCpp.cpp
+++ AgeFibNumCpp.cpp
@@ -1,55 +1,39 @@
 #include <stdio.h>
-#include <iostream>
-using namespace std;
 
-
-int valid_age(int age) 
-{
-	if(age == 0)
-	{
+int valid_age(int age){
+	if(age == 0){
 		printf("Insert a valid number \n");
 	}		
-
 	return 0;
 }
 
-void Binary(int sum)
-{
+void Binary(int sum){
 	int Binary=0;
-   	printf("The binary representation of the sum %d is :", sum);
-	printf("\n");
-	while(sum>0)
-	{
-		Binary = sum%2;
-		sum =sum/2;
-		printf("%d\n",Binary);
-		
+	printf("the binary representation of the sum %d is:",sum);
+	for(int bit=31;bit>=0;bit--){
+		Binary = sum>>bit;
+		if (Binary & 1)
+			printf("1");
+		else
+			printf("0");
 	}
-
+	printf("\n");
 }
 
-int main()
-{
+int main(){
 	int age;
-	cout<<" Please insert your age: ";
-	cin>>age;
-                int temp,sum = 0;
-		int fib1=0;
-		int fib2 = 1;
-	
-		while(fib2 < age)
-	
-		{
+	printf(" Please insert your age: ");
+	scanf("%d",&age);
+                int temp,sum = 0, fib1=0,fib2 = 1;
+		while(fib2 < age){
 			temp = fib1;
 			fib1 = fib2;
 		      	fib2 =fib2 +temp;
 			sum =sum + fib1;
 		}
-		printf("The sum of the Fibo numbers which are less than your age %d is : %d \n",age, sum);
-		
+		printf("The sum of the Fibo numbers which are less than your age %d is : %d \n",age, sum);		
 	valid_age(age);
 	Binary(sum);
-
 	return 0;
 }
 
